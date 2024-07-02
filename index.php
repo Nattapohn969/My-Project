3fa77583c4bb1cf66e8df161d810c59434902519
Natta <137468890+Nattapohn969@users.noreply.github.com>
2024-07-02 22:45:30 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+include "connect.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <title>Market</title>
+</head>
+<body>
+    
+</body>
+</html>
